38801211ffa4ec7dc44ca973395f8e40858d9ee6
刘烽 <feng.liu01@hand-china.com>
2020-04-20 16:35:19 +0800

    [UPDATE]启动脚本更新

--- scripts/hdsp-quality-startup.sh
+++ scripts/hdsp-quality-startup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+appName=hdsp-quality
 source /etc/profile
 exec nohup java -Xmx500m -Xms500m \
-    -DSPRING_DATASOURCE_URL='jdbc:mysql://db.hdsp.hand.com:3306/hdsp_quality?useUnicode=true&characterEncoding=utf-8&useSSL=false' \
-    -DSPRING_DATASOURCE_USERNAME='hdsp_dev' \
-    -DSPRING_DATASOURCE_PASSWORD='hdsp_dev' \
-	  -DSPRING_REDIS_HOST='redis.hdsp.hand.com' \
-    -DSPRING_REDIS_PORT=6379 \
-    -DSPRING_REDIS_PASSWORD='hdsp_dev' \
-    -jar hdsp-quality.jar > ./logs/hdsp-quality.log 2>&1 &
+  -DSPRING_CLOUD_CONFIG_ENABLED=true \
+  -DSPRING_PROFILES_ACTIVE=default \
+  -DSPRING_DATASOURCE_URL='jdbc:mysql://ip:23306/hdsp_core?useUnicode=true&characterEncoding=utf-8&useSSL=false' \
+  -DSPRING_DATASOURCE_USERNAME='username' \
+  -DSPRING_DATASOURCE_PASSWORD='hpassword' \
+  -DSPRING_REDIS_HOST='ip' \
+  -DSPRING_REDIS_PORT=port \
+  -DSPRING_REDIS_PASSWORD='password' \
+  -DHDSP_ACCESS_SIGN_ENABLED=false \
+  -DHDSP_ACCESS_SIGN_URL_SUFFIX='/v2/.*' \
+  -DHDSP_ACCESS_SIGN_EXCLUDE_URL_SUFFIX ='/v1/.*' \
+  -DHDSP_ACCESS_SIGN_INIT_TOKEN_NUM=10 \
+  -jar $appName.jar >./logs/$appName.log 2>&1 &
