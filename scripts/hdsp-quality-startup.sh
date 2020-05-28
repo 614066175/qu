b1abadb62597def34e86a7b0afb524f10afb9384
刘烽 <feng.liu01@hand-china.com>
2020-05-28 14:22:20 +0800

    [UPDATE]启动脚本更新

--- scripts/hdsp-quality-startup.sh
+++ scripts/hdsp-quality-startup.sh
@@ -5,12 +5,14 @@ source /etc/profile
 exec nohup java -Xmx500m -Xms500m \
   -DSPRING_CLOUD_CONFIG_ENABLED=true \
   -DSPRING_PROFILES_ACTIVE=default \
-  -DSPRING_DATASOURCE_URL='jdbc:mysql://ip:23306/hdsp_core?useUnicode=true&characterEncoding=utf-8&useSSL=false' \
+  -DSPRING_DATASOURCE_URL='jdbc:mysql://ip:23306/hdsp_quality?useUnicode=true&characterEncoding=utf-8&useSSL=false' \
   -DSPRING_DATASOURCE_USERNAME='username' \
   -DSPRING_DATASOURCE_PASSWORD='hpassword' \
   -DSPRING_REDIS_HOST='ip' \
   -DSPRING_REDIS_PORT=port \
   -DSPRING_REDIS_PASSWORD='password' \
+  -DSPRING_CLOUD_CONFIG_URI='config' \
+  -DEUREKA_DEFAULT_ZONE='eureka' \
   -DHDSP_ACCESS_SIGN_ENABLED=false \
   -DHDSP_ACCESS_SIGN_URL_SUFFIX='/v2/.*' \
   -DHDSP_ACCESS_SIGN_EXCLUDE_URL_SUFFIX ='/v1/.*' \
