1eb8e245a06119177ee4af56bcc17d755e6b6870
李国梁 <guoliang.li01@hand-china.com>
2020-12-13 21:22:03 +0800

    [fix] fix bug

---
+++ src/main/java/com/hand/hdsp/quality/api/dto/TableRelCheckDTO.java
@@ -0,0 +1,40 @@
+package com.hand.hdsp.quality.api.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+/**
+ * <p>
+ * description
+ * </p>
+ *
+ * @author lgl 2020/12/11 17:36
+ * @since 1.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ApiModel("表间计算值比较规则")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TableRelCheckDTO {
+
+
+    private String sourceFunction;
+
+    private String sourceFieldName;
+
+    @ApiModelProperty(value = "关联关系 HDSP.XQUA.REL_CODE")
+    @NotBlank
+    private String relCode;
+
+    private String relFunction;
+
+    private String relFieldName;
+
+}
--- src/main/java/com/hand/hdsp/quality/infra/measure/measure/RelTableMeasure.java
+++ src/main/java/com/hand/hdsp/quality/infra/measure/measure/RelTableMeasure.java
@@ -18,6 +18,7 @@ import com.hand.hdsp.quality.infra.measure.CheckItem;
 import com.hand.hdsp.quality.infra.measure.Measure;
 import com.hand.hdsp.quality.infra.measure.MeasureUtil;
 import com.hand.hdsp.quality.infra.util.JsonUtils;
+import com.hand.hdsp.quality.infra.vo.WarningLevelVO;
 import io.choerodon.core.exception.CommonException;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -35,7 +36,8 @@ public class RelTableMeasure implements Measure {
 
     private final ItemTemplateSqlRepository templateSqlRepository;
     private final DriverSessionService driverSessionService;
-    private static final String SQL = "SELECT count(*) count FROM %s source WHERE EXISTS (SELECT 1 FROM %s.%s rel WHERE %s) and %s";
+    private static final String ACCURACY_RATE_SQL = "SELECT count(*) count FROM %s source WHERE EXISTS (SELECT 1 FROM %s.%s rel WHERE %s) and %s";
+    private static final String CALCULATED_VALUE_SQL = "select count(*) COUNT from (select %s,%s as value1 from %s) source inner join (select %s ,%s from %s.%s ) rel on %s";
 
     public RelTableMeasure(ItemTemplateSqlRepository templateSqlRepository, DriverSessionService driverSessionService) {
         this.templateSqlRepository = templateSqlRepository;
@@ -50,71 +52,93 @@ public class RelTableMeasure implements Measure {
         List<RelationshipDTO> relationshipDTOList = JsonUtils.json2Relationship(batchPlanRelTable.getRelationship());
         List<WarningLevelDTO> warningLevelList = JsonUtils.json2WarningLevel(batchPlanRelTable.getWarningLevel());
         BatchResultItem batchResultItem = param.getBatchResultItem();
+        if (PlanConstant.CheckItem.ACCURACY_RATE.equals(param.getCheckItem())) {
+            StringBuilder where = new StringBuilder();
+            where.append(" 1 = 1 ");
+            if (CollectionUtils.isNotEmpty(relationshipDTOList)) {
+                for (RelationshipDTO relationshipDTO : relationshipDTOList) {
+                    where.append(" and source.").append(relationshipDTO.getSourceFieldName())
+                            .append(relationshipDTO.getRelCode())
+                            .append(" rel.").append(relationshipDTO.getRelFieldName());
+                }
+            }
+            if (StringUtils.isNotBlank(batchPlanRelTable.getWhereCondition())) {
+                where.append(" and ").append(batchPlanRelTable.getWhereCondition());
+            }
+            DriverSession driverSession = driverSessionService.getDriverSession(tenantId, param.getPluginDatasourceDTO().getDatasourceCode());
+            List<Map<String, Object>> result = driverSession.executeOneQuery(param.getSchema(),
+                    String.format(ACCURACY_RATE_SQL, batchResultBase.getPackageObjectName(), batchPlanRelTable.getRelSchema(),
+                            batchPlanRelTable.getRelTableName(), where.toString(), Objects.isNull(batchResultBase.getWhereCondition()) ? "1=1" : batchPlanRelTable.getWhereCondition()));
+            //获取表行数
+            ItemTemplateSql itemTemplateSql = templateSqlRepository.selectSql(ItemTemplateSql.builder()
+                    .checkItem(PlanConstant.CheckItem.TABLE_LINE)
+                    .datasourceType(batchResultBase.getDatasourceType())
+                    .build());
 
+            Map<String, String> variables = new HashMap<>(5);
+            variables.put("table", batchPlanRelTable.getRelSchema() + "." + batchPlanRelTable.getRelTableName());
 
-        StringBuilder where = new StringBuilder();
-        where.append(" 1 = 1 ");
-        if (CollectionUtils.isNotEmpty(relationshipDTOList)) {
-            for (RelationshipDTO relationshipDTO : relationshipDTOList) {
-                where.append(" and source.").append(relationshipDTO.getSourceFieldName())
-                        .append(relationshipDTO.getRelCode())
-                        .append(" rel.").append(relationshipDTO.getRelFieldName());
+            List<Map<String, Object>> response = driverSession.executeOneQuery(param.getSchema(),
+                    MeasureUtil.replaceVariable(itemTemplateSql.getSqlContent(), variables, batchPlanRelTable.getWhereCondition()));
+            if (response.size() != 1 || response.get(0).size() != 1) {
+                throw new CommonException(ErrorCode.CHECK_ITEM_ONE_VALUE);
+            }
+
+            long dataCount = Long.parseLong(String.valueOf(response.get(0).values().toArray()[0]));
+
+            //计算准确率
+            BigDecimal a = new BigDecimal(String.valueOf(Optional.ofNullable(result.get(0).get("count")).orElse(result.get(0).get("COUNT"))));
+            BigDecimal b = BigDecimal.valueOf(dataCount);
+            BigDecimal rate = BigDecimal.ZERO;
+            if (BigDecimal.ZERO.compareTo(b) != 0) {
+                rate = a.divide(b, 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
+            }
+
+            batchResultItem.setActualValue(rate.toString() + BaseConstants.Symbol.PERCENTAGE);
+            batchResultItem.setCurrentValue(rate.toString());
+            batchResultItem.setRelTableName(batchPlanRelTable.getRelTableName());
+
+            for (WarningLevelDTO warningLevel : warningLevelList) {
+                String startValue = warningLevel.getStartValue();
+                String endValue = warningLevel.getEndValue();
+                if (StringUtils.isBlank(startValue) && StringUtils.isBlank(endValue)) {
+                    throw new CommonException(ErrorCode.WARNING_LEVEL_RANGE_NOT_ALL_EMPTY);
+                }
+                boolean startResult = true;
+                boolean endResult = true;
+                if (StringUtils.isNotBlank(startValue)) {
+                    startResult = new BigDecimal(startValue).compareTo(rate) <= 0;
+                }
+                if (StringUtils.isNotBlank(endValue)) {
+                    endResult = new BigDecimal(endValue).compareTo(rate) >= 0;
+                }
+                if (startResult && endResult) {
+                    batchResultItem.setWarningLevel(JsonUtils.object2Json(WarningLevelVO.builder()
+                            .warningLevel(warningLevel.getWarningLevel())
+                            .build()));
+                    batchResultItem.setExceptionInfo("准确率（目标表与源表匹配条数/源表总条数）在告警范围内");
+                }
             }
         }
-        if (StringUtils.isNotBlank(batchPlanRelTable.getWhereCondition())) {
-            where.append(" and ").append(batchPlanRelTable.getWhereCondition());
-        }
-        DriverSession driverSession = driverSessionService.getDriverSession(tenantId, param.getPluginDatasourceDTO().getDatasourceCode());
-        List<Map<String, Object>> result = driverSession.executeOneQuery(param.getSchema(),
-                String.format(SQL, batchResultBase.getPackageObjectName(), batchPlanRelTable.getRelSchema(),
-                        batchPlanRelTable.getRelTableName(), where.toString(), Objects.isNull(batchResultBase.getWhereCondition()) ? "1=1" : batchPlanRelTable.getWhereCondition()));
-        //获取表行数
-        ItemTemplateSql itemTemplateSql = templateSqlRepository.selectSql(ItemTemplateSql.builder()
-                .checkItem(PlanConstant.CheckItem.TABLE_LINE)
-                .datasourceType(batchResultBase.getDatasourceType())
-                .build());
-
-        Map<String, String> variables = new HashMap<>(5);
-        variables.put("table", batchPlanRelTable.getRelSchema() + "." + batchPlanRelTable.getRelTableName());
-
-        List<Map<String, Object>> response = driverSession.executeOneQuery(param.getSchema(),
-                MeasureUtil.replaceVariable(itemTemplateSql.getSqlContent(), variables, batchPlanRelTable.getWhereCondition()));
-        if (response.size() != 1 || response.get(0).size() != 1) {
-            throw new CommonException(ErrorCode.CHECK_ITEM_ONE_VALUE);
-        }
-
-        long dataCount = Long.parseLong(String.valueOf(response.get(0).values().toArray()[0]));
-
-        //计算准确率
-        BigDecimal a = new BigDecimal(String.valueOf(Optional.ofNullable(result.get(0).get("count")).orElse(result.get(0).get("COUNT"))));
-        BigDecimal b = BigDecimal.valueOf(dataCount);
-        BigDecimal rate = BigDecimal.ZERO;
-        if (BigDecimal.ZERO.compareTo(b) != 0) {
-            rate = a.divide(b, 2, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
-        }
-
-        batchResultItem.setActualValue(rate.toString() + BaseConstants.Symbol.PERCENTAGE);
-        batchResultItem.setCurrentValue(rate.toString());
-        batchResultItem.setRelTableName(batchPlanRelTable.getRelTableName());
-
-        for (WarningLevelDTO warningLevel : warningLevelList) {
-            String startValue = warningLevel.getStartValue();
-            String endValue = warningLevel.getEndValue();
-            if (StringUtils.isBlank(startValue) && StringUtils.isBlank(endValue)) {
-                throw new CommonException(ErrorCode.WARNING_LEVEL_RANGE_NOT_ALL_EMPTY);
+        //计算值比较
+        else if (PlanConstant.CheckItem.CALCULATED_VALUE.equals(param.getCheckItem())) {
+            StringBuilder where = new StringBuilder();
+            where.append(" 1 = 1 ");
+            if (CollectionUtils.isNotEmpty(relationshipDTOList)) {
+                for (RelationshipDTO relationshipDTO : relationshipDTOList) {
+                    where.append(" and source.").append(relationshipDTO.getSourceFieldName())
+                            .append(relationshipDTO.getRelCode())
+                            .append(" rel.").append(relationshipDTO.getRelFieldName());
+                }
             }
-            boolean startResult = true;
-            boolean endResult = true;
-            if (StringUtils.isNotBlank(startValue)) {
-                startResult = new BigDecimal(startValue).compareTo(rate) <= 0;
-            }
-            if (StringUtils.isNotBlank(endValue)) {
-                endResult = new BigDecimal(endValue).compareTo(rate) >= 0;
-            }
-            if (startResult && endResult) {
-                batchResultItem.setWarningLevel(warningLevel.getWarningLevel());
-                batchResultItem.setExceptionInfo("准确率（目标表与源表匹配条数/源表总条数）在告警范围内");
-            }
+            DriverSession driverSession = driverSessionService.getDriverSession(tenantId, param.getPluginDatasourceDTO().getDatasourceCode());
+            String sql = String.format(CALCULATED_VALUE_SQL,
+                    batchResultBase.getPackageObjectName(),
+                    batchPlanRelTable.getRelSchema(),
+                    batchPlanRelTable.getRelTableName(),
+                    where.toString(),
+                    Objects.isNull(batchResultBase.getWhereCondition()) ? "1=1" : batchPlanRelTable.getWhereCondition());
+            List<Map<String, Object>> result = driverSession.executeOneQuery(param.getSchema(), sql);
         }
 
         return batchResultItem;
