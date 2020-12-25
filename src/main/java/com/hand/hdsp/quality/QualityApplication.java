37f65578ae650a44fd0e8a116c92a851bc02174d
dengzhilong <zhilong.deng@hand-china.com>
2020-12-25 17:53:18 +0800

    [UPDATE]扫描jar的bean

--- src/main/java/com/hand/hdsp/quality/QualityApplication.java
+++ src/main/java/com/hand/hdsp/quality/QualityApplication.java
@@ -3,12 +3,14 @@ package com.hand.hdsp.quality;
 import com.hand.hdsp.quality.infra.annotation.EnableQuality;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author aaron.yi
  */
 @SpringBootApplication
 @EnableQuality
+@ComponentScan("com.hand.hdsp.*")
 public class QualityApplication {
 
     public static void main(String[] args) {
