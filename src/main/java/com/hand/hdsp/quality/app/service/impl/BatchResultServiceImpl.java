f6a817c4b6d8983a8689a027413f489c9ff4eccb
李国梁 <guoliang.li01@hand-china.com>
2021-01-05 14:37:58 +0800

    [add] 异常数据添加查询

--- src/main/java/com/hand/hdsp/quality/app/service/impl/BatchResultServiceImpl.java
+++ src/main/java/com/hand/hdsp/quality/app/service/impl/BatchResultServiceImpl.java
@@ -22,6 +22,7 @@ import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.exception.ExceptionResponse;
 import io.choerodon.mybatis.pagehelper.domain.PageRequest;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.util.Strings;
 import org.hzero.core.util.ResponseUtils;
 import org.hzero.mybatis.domian.Condition;
 import org.hzero.mybatis.util.Sqls;
@@ -163,6 +164,9 @@ public class BatchResultServiceImpl implements BatchResultService {
                 });
                 //设置到每个base中
                 batchResultBaseDTO.setResultWaringVOList(resultWaringVOList);
+                if(Objects.isNull(batchResultBaseDTO.getDataCount())){
+                    batchResultBaseDTO.setDataCount(0L);
+                }
                 page.setContent(batchResultBaseDTOList);
             }
         }
@@ -177,7 +181,20 @@ public class BatchResultServiceImpl implements BatchResultService {
         //查看base的异常数据 目前只有字段级有异常数据
         Object json = redisTemplate.opsForHash().get(String.format("%s:%d", PlanConstant.CACHE_BUCKET_EXCEPTION,
                 exceptionDataDTO.getPlanBaseId()), PlanConstant.ResultRuleType.FIELD);
-        return (List<Map<String, Object>>) JSONArray.parse(String.valueOf(json));
+        List<Map<String, Object>> result = (List<Map<String, Object>>) JSONArray.parse(String.valueOf(json));
+        if(Strings.isNotEmpty(exceptionDataDTO.getRuleName())){
+            result = result.stream().filter(map -> {
+                String ruleName = String.valueOf(map.get("$ruleName"));
+                return ruleName.contains(exceptionDataDTO.getRuleName());
+            }).collect(Collectors.toList());
+        }
+        if(Strings.isNotEmpty(exceptionDataDTO.getWarningLevel())){
+            result = result.stream().filter(map -> {
+                String warningLevel = String.valueOf(map.get("$warningLevel"));
+                return exceptionDataDTO.getWarningLevel().equals(warningLevel);
+            }).collect(Collectors.toList());
+        }
+        return result;
     }
 
 }
