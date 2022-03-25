b16af72149a5fcd35fbe7cdf6eb83d51dc9ae200
dongwl <3047409991@qq.com>
2022-03-25 15:32:52 +0800

    [update] 新建删除标准分组

--- src/main/java/com/hand/hdsp/quality/app/service/impl/StandardGroupServiceImpl.java
+++ src/main/java/com/hand/hdsp/quality/app/service/impl/StandardGroupServiceImpl.java
@@ -2,14 +2,10 @@ package com.hand.hdsp.quality.app.service.impl;
 
 import java.util.List;
 
-import com.hand.hdsp.quality.api.dto.DataStandardDTO;
-import com.hand.hdsp.quality.api.dto.StandardGroupDTO;
+import com.hand.hdsp.quality.api.dto.*;
 import com.hand.hdsp.quality.app.service.StandardGroupService;
-import com.hand.hdsp.quality.domain.entity.DataStandard;
-import com.hand.hdsp.quality.domain.entity.StandardGroup;
-import com.hand.hdsp.quality.domain.repository.DataStandardRepository;
-import com.hand.hdsp.quality.domain.repository.StandardDocRepository;
-import com.hand.hdsp.quality.domain.repository.StandardGroupRepository;
+import com.hand.hdsp.quality.domain.entity.*;
+import com.hand.hdsp.quality.domain.repository.*;
 import com.hand.hdsp.quality.infra.constant.ErrorCode;
 import com.hand.hdsp.quality.infra.constant.StandardConstant;
 import com.hand.hdsp.quality.infra.mapper.DataStandardMapper;
@@ -47,13 +43,18 @@ public class StandardGroupServiceImpl implements StandardGroupService {
 
     private final DataStandardMapper dataStandardMapper;
 
+    private final NameStandardRepository nameStandardRepository;
 
-    public StandardGroupServiceImpl(StandardGroupRepository standardGroupRepository, StandardGroupMapper standardGroupMapper, DataStandardRepository dataStandardRepository, StandardDocRepository standardDocRepository, DataStandardMapper dataStandardMapper) {
+    private final DataFieldRepository dataFieldRepository;
+
+    public StandardGroupServiceImpl(StandardGroupRepository standardGroupRepository, StandardGroupMapper standardGroupMapper, DataStandardRepository dataStandardRepository, StandardDocRepository standardDocRepository, DataStandardMapper dataStandardMapper, NameStandardRepository nameStandardRepository, DataFieldRepository dataFieldRepository) {
         this.standardGroupRepository = standardGroupRepository;
         this.standardGroupMapper = standardGroupMapper;
         this.dataStandardRepository = dataStandardRepository;
         this.standardDocRepository = standardDocRepository;
         this.dataStandardMapper = dataStandardMapper;
+        this.nameStandardRepository = nameStandardRepository;
+        this.dataFieldRepository = dataFieldRepository;
     }
 
     @Override
@@ -65,19 +66,64 @@ public class StandardGroupServiceImpl implements StandardGroupService {
     public void delete(StandardGroupDTO standardGroupDTO) {
         //判断目录是否包含子目录
         List<StandardGroupDTO> standardGroups = standardGroupRepository.selectDTOByCondition(Condition.builder(StandardGroup.class)
-                .andWhere(Sqls.custom().andEqualTo(StandardGroup.FIELD_PARENT_GROUP_ID, standardGroupDTO.getGroupId()))
+                .andWhere(Sqls.custom()
+                        .andEqualTo(StandardGroup.FIELD_PARENT_GROUP_ID, standardGroupDTO.getGroupId())
+                        .andEqualTo(StandardGroup.FIELD_TENANT_ID, standardGroupDTO.getTenantId()))
                 .build());
         if (CollectionUtils.isNotEmpty(standardGroups)) {
             throw new CommonException(ErrorCode.GROUP_HAS_CHILD_GROUP);
         }
         //判断是否包含标准
-        List<DataStandardDTO> dataStandardDTOS = dataStandardRepository.selectDTOByCondition(Condition.builder(DataStandard.class)
-                .andWhere(Sqls.custom().andEqualTo(DataStandard.FIELD_GROUP_ID, standardGroupDTO.getGroupId()))
-                .build());
-        if (CollectionUtils.isNotEmpty(dataStandardDTOS)) {
-            throw new CommonException(ErrorCode.GROUP_HAS_STANDARD);
-        }
+        existStandard(standardGroupDTO);
         standardGroupRepository.deleteByPrimaryKey(standardGroupDTO);
+    }
+
+    private void existStandard(StandardGroupDTO standardGroupDTO) {
+        String standardType = standardGroupDTO.getStandardType();
+        switch (standardType) {
+            case StandardConstant.StandardType.DATA:
+                List<DataStandardDTO> dataStandardDTOS = dataStandardRepository.selectDTOByCondition(Condition.builder(DataStandard.class)
+                        .andWhere(Sqls.custom()
+                                .andEqualTo(DataStandard.FIELD_GROUP_ID, standardGroupDTO.getGroupId())
+                                .andEqualTo(DataStandard.FIELD_TENANT_ID, standardGroupDTO.getTenantId()))
+                        .build());
+                if (CollectionUtils.isNotEmpty(dataStandardDTOS)) {
+                    throw new CommonException(ErrorCode.GROUP_HAS_STANDARD);
+                }
+                break;
+            case StandardConstant.StandardType.FIELD:
+                List<DataFieldDTO> dataFieldDTOS = dataFieldRepository.selectDTOByCondition(Condition.builder(DataField.class)
+                        .andWhere(Sqls.custom()
+                                .andEqualTo(DataField.FIELD_GROUP_ID, standardGroupDTO.getGroupId())
+                                .andEqualTo(DataField.FIELD_TENANT_ID, standardGroupDTO.getTenantId()))
+                        .build());
+                if (CollectionUtils.isNotEmpty(dataFieldDTOS)) {
+                    throw new CommonException(ErrorCode.GROUP_HAS_STANDARD);
+                }
+                break;
+            case StandardConstant.StandardType.NAME:
+                List<NameStandardDTO> nameStandardDTOS = nameStandardRepository.selectDTOByCondition(Condition.builder(NameStandard.class)
+                        .andWhere(Sqls.custom()
+                                .andEqualTo(NameStandard.FIELD_GROUP_ID, standardGroupDTO.getGroupId())
+                                .andEqualTo(NameStandard.FIELD_TENANT_ID, standardGroupDTO.getTenantId()))
+                        .build());
+                if (CollectionUtils.isNotEmpty(nameStandardDTOS)) {
+                    throw new CommonException(ErrorCode.GROUP_HAS_STANDARD);
+                }
+                break;
+            case StandardConstant.StandardType.DOC:
+                List<StandardDocDTO> standardDocDTOS = standardDocRepository.selectDTOByCondition(Condition.builder(StandardDoc.class)
+                        .andWhere(Sqls.custom()
+                                .andEqualTo(StandardDoc.FIELD_GROUP_ID, standardGroupDTO.getGroupId())
+                                .andEqualTo(StandardDoc.FIELD_TENANT_ID, standardGroupDTO.getTenantId()))
+                        .build());
+                if (CollectionUtils.isNotEmpty(standardDocDTOS)) {
+                    throw new CommonException(ErrorCode.GROUP_HAS_STANDARD);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -128,6 +174,12 @@ public class StandardGroupServiceImpl implements StandardGroupService {
 
     @Override
     public int create(StandardGroupDTO standardGroupDTO) {
+        // 校验父目录下是否有标准
+        if (standardGroupDTO.getParentGroupId() != null) {
+            StandardGroupDTO dto = standardGroupRepository.selectDTOByPrimaryKey(standardGroupDTO.getParentGroupId());
+            existStandard(dto);
+        }
+        // 校验编码存在
         List<StandardGroupDTO> dtoList = standardGroupRepository.selectDTOByCondition(Condition.builder(StandardGroup.class)
                 .andWhere(Sqls.custom()
                         .andEqualTo(StandardGroup.FIELD_TENANT_ID, standardGroupDTO.getTenantId())
@@ -137,6 +189,7 @@ public class StandardGroupServiceImpl implements StandardGroupService {
         if (CollectionUtils.isNotEmpty(dtoList)) {
             throw new CommonException(ErrorCode.CODE_ALREADY_EXISTS);
         }
+        // 校验名称存在
         dtoList = standardGroupRepository.selectDTOByCondition(Condition.builder(StandardGroup.class)
                 .andWhere(Sqls.custom()
                         .andEqualTo(StandardGroup.FIELD_TENANT_ID, standardGroupDTO.getTenantId())
