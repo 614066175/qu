cdacf0f2f52725164837bf447b5beb20b55cbff5
goshJ <shijie.gao@hand-china.com>
2022-11-28 20:01:49 +0800

    [add] 字段标准配置项校验导入

--- src/main/java/com/hand/hdsp/quality/infra/batchimport/DataFieldStandardBatchImportServiceImpl.java
+++ src/main/java/com/hand/hdsp/quality/infra/batchimport/DataFieldStandardBatchImportServiceImpl.java
@@ -6,21 +6,24 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hand.hdsp.core.util.ProjectHelper;
-import com.hand.hdsp.quality.api.dto.DataFieldDTO;
-import com.hand.hdsp.quality.api.dto.StandardGroupDTO;
+import com.hand.hdsp.quality.api.dto.*;
+import com.hand.hdsp.quality.domain.entity.DataStandard;
 import com.hand.hdsp.quality.domain.entity.StandardGroup;
-import com.hand.hdsp.quality.domain.repository.DataFieldRepository;
-import com.hand.hdsp.quality.domain.repository.StandardGroupRepository;
+import com.hand.hdsp.quality.domain.entity.StandardTeam;
+import com.hand.hdsp.quality.domain.repository.*;
 import com.hand.hdsp.quality.infra.constant.TemplateCodeConstants;
 import com.hand.hdsp.quality.infra.mapper.DataFieldMapper;
 import io.choerodon.core.oauth.DetailsHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hzero.boot.imported.app.service.IBatchImportService;
 import org.hzero.boot.imported.infra.validator.annotation.ImportService;
 import org.hzero.mybatis.domian.Condition;
 import org.hzero.mybatis.helper.DataSecurityHelper;
 import org.hzero.mybatis.util.Sqls;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.hand.hdsp.quality.infra.constant.StandardConstant.StandardType.FIELD;
 import static com.hand.hdsp.quality.infra.constant.StandardConstant.Status.CREATE;
@@ -29,25 +32,35 @@ import static com.hand.hdsp.quality.infra.constant.StandardConstant.Status.CREAT
  * @author wsl
  */
 @Slf4j
-@ImportService(templateCode = TemplateCodeConstants.TEMPLATE_CODE_FIELD_STANDARD,sheetIndex = 1)
+@ImportService(templateCode = TemplateCodeConstants.TEMPLATE_CODE_FIELD_STANDARD, sheetIndex = 1)
 public class DataFieldStandardBatchImportServiceImpl implements IBatchImportService {
 
     private final ObjectMapper objectMapper;
     private final DataFieldRepository dataFieldRepository;
     private final StandardGroupRepository standardGroupRepository;
     private final DataFieldMapper dataFieldMapper;
+    private final DataStandardRepository dataStandardRepository;
+    private final StandardRelationRepository standardRelationRepository;
+    private final StandardTeamRepository standardTeamRepository;
 
     public DataFieldStandardBatchImportServiceImpl(ObjectMapper objectMapper,
                                                    DataFieldRepository dataFieldRepository,
                                                    StandardGroupRepository standardGroupRepository,
-                                                   DataFieldMapper dataFieldMapper) {
+                                                   DataFieldMapper dataFieldMapper,
+                                                   DataStandardRepository dataStandardRepository,
+                                                   StandardRelationRepository standardRelationRepository,
+                                                   StandardTeamRepository standardTeamRepository) {
         this.objectMapper = objectMapper;
         this.dataFieldRepository = dataFieldRepository;
         this.standardGroupRepository = standardGroupRepository;
         this.dataFieldMapper = dataFieldMapper;
+        this.dataStandardRepository = dataStandardRepository;
+        this.standardRelationRepository = standardRelationRepository;
+        this.standardTeamRepository = standardTeamRepository;
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Boolean doImport(List<String> data) {
         // 设置租户Id
         Long tenantId = DetailsHelper.getUserDetails().getTenantId();
@@ -61,24 +74,57 @@ public class DataFieldStandardBatchImportServiceImpl implements IBatchImportServ
                 List<StandardGroupDTO> standardGroupDTOList = standardGroupRepository.selectDTOByCondition(Condition.builder(StandardGroup.class).andWhere(Sqls.custom()
                         .andEqualTo(StandardGroup.FIELD_GROUP_CODE, groupCode)
                         .andEqualTo(StandardGroup.FIELD_STANDARD_TYPE, FIELD)
-                        .andEqualTo(StandardGroup.FIELD_TENANT_ID,tenantId)
-                        .andEqualTo(StandardGroup.FIELD_PROJECT_ID,projectId)
+                        .andEqualTo(StandardGroup.FIELD_TENANT_ID, tenantId)
+                        .andEqualTo(StandardGroup.FIELD_PROJECT_ID, projectId)
                 ).build());
-                if(CollectionUtils.isNotEmpty(standardGroupDTOList)){
+                if (CollectionUtils.isNotEmpty(standardGroupDTOList)) {
                     dataFieldDTO.setGroupId(standardGroupDTOList.get(0).getGroupId());
                 }
                 dataFieldDTO.setTenantId(tenantId);
                 dataFieldDTO.setProjectId(projectId);
                 dataFieldDTO.setStandardStatus(CREATE);
                 //设置责任人
-                if(DataSecurityHelper.isTenantOpen()){
+                if (DataSecurityHelper.isTenantOpen()) {
                     //加密后查询
                     String chargeName = DataSecurityHelper.encrypt(dataFieldDTO.getChargeName());
                     dataFieldDTO.setChargeName(chargeName);
                 }
                 Long chargeId = dataFieldMapper.checkCharger(dataFieldDTO.getChargeName(), dataFieldDTO.getTenantId());
                 dataFieldDTO.setChargeId(chargeId);
-                dataFieldDTOList.add(dataFieldDTO);
+                //设置目标环境引用数据标准
+                if (StringUtils.isNotEmpty(dataFieldDTO.getDataStandardCode())) {
+                    List<DataStandardDTO> dataStandardDTOList = dataStandardRepository.selectDTOByCondition(Condition.builder(DataStandard.class).andWhere(Sqls.custom()
+                                    .andEqualTo(DataStandard.FIELD_TENANT_ID, tenantId)
+                                    .andEqualTo(DataStandard.FIELD_PROJECT_ID, projectId)
+                                    .andEqualTo(DataStandard.FIELD_STANDARD_CODE, dataFieldDTO.getDataStandardCode()))
+                            .build());
+                    if (CollectionUtils.isNotEmpty(dataStandardDTOList)) {
+                        dataFieldDTO.setDataStandardId(dataStandardDTOList.get(0).getStandardId());
+                    }
+                }
+                dataFieldRepository.insertDTOSelective(dataFieldDTO);
+                //设置字段标准组
+                if (StringUtils.isNotEmpty(dataFieldDTO.getStandardTeamCode())) {
+                    String[] standardTeamCodeList = dataFieldDTO.getStandardTeamCode().split(",");
+                    Long fieldId = dataFieldDTO.getFieldId();
+                    for (String standardTeamCode : standardTeamCodeList) {
+                        List<StandardTeamDTO> standardTeamDTOS = standardTeamRepository.selectDTOByCondition(Condition.builder(StandardTeam.class).andWhere(Sqls.custom()
+                                        .andEqualTo(StandardTeam.FIELD_STANDARD_TEAM_CODE, standardTeamCode))
+                                .build());
+                        if (CollectionUtils.isNotEmpty(standardTeamDTOS)) {
+                            Long standardTeamId = standardTeamDTOS.get(0).getStandardTeamId();
+                            if (ObjectUtils.isNotEmpty(standardTeamId)) {
+                                //关联导入环境 - 字段标准组，字段标准
+                                standardRelationRepository.insertDTOSelective(StandardRelationDTO.builder()
+                                        .tenantId(tenantId)
+                                        .projectId(projectId)
+                                        .standardTeamId(standardTeamId)
+                                        .fieldStandardId(fieldId)
+                                        .build());
+                            }
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             // 失败
@@ -86,7 +132,6 @@ public class DataFieldStandardBatchImportServiceImpl implements IBatchImportServ
             log.error("StandardDoc Object Read Json Error", e);
             return false;
         }
-        dataFieldRepository.batchInsertDTOSelective(dataFieldDTOList);
         return true;
     }
 }
