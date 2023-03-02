ff090125bb175d3a0cb110f146dff3e24175c09c
盛鑫 <xin.sheng01@china-hand.com>
2023-03-02 16:28:48 +0800

    [update] 无空值校验默认值设置

---
+++ src/main/java/com/hand/hdsp/quality/infra/constant/CheckConstants.java
@@ -0,0 +1,28 @@
+package com.hand.hdsp.quality.infra.constant;
+
+/**
+ * description
+ *
+ * @author XIN.SHENG01@HAND-CHINA.COM 2023/03/02 15:55
+ */
+public interface CheckConstants {
+    /**
+     * 无空值
+     */
+    String NOT_EMPTY = "FIELD_NOT_EMPTY";
+
+    /**
+     *值比较
+     */
+    String COMMON = "COMMON";
+
+    /**
+     * 固定值
+     */
+    String FIXED_VALUE ="FIXED_VALUE";
+
+    /**
+     * 值比较
+     */
+    String VALUE = "VALUE";
+}
