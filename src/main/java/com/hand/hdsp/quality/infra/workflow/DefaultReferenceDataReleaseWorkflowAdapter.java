b248dfcc323af996e615b1df3f0b3168241f22a1
29702 <fuqiang.luo@hand-china.com>
2023-05-19 16:17:13 +0800

    [add] 参考数据发布&下线流程操作加锁防止用户连续点击发布或者下线按钮造成发起多个流程

--- src/main/java/com/hand/hdsp/quality/infra/workflow/DefaultReferenceDataReleaseWorkflowAdapter.java
+++ src/main/java/com/hand/hdsp/quality/infra/workflow/DefaultReferenceDataReleaseWorkflowAdapter.java
@@ -1,6 +1,8 @@
 package com.hand.hdsp.quality.infra.workflow;
 
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.hand.hdsp.core.domain.entity.CommonGroup;
 import com.hand.hdsp.core.domain.repository.CommonGroupRepository;
@@ -42,6 +44,7 @@ public class DefaultReferenceDataReleaseWorkflowAdapter implements ReferenceData
     private final ReferenceDataHistoryRepository referenceDataHistoryRepository;
     private final ReferenceDataValueRepository referenceDataValueRepository;
     private final CommonGroupRepository commonGroupRepository;
+    private final Lock lock = new ReentrantLock();
 
     public DefaultReferenceDataReleaseWorkflowAdapter(WorkflowClient workflowClient,
                                                       ReferenceDataRecordRepository referenceDataRecordRepository,
@@ -60,49 +63,54 @@ public class DefaultReferenceDataReleaseWorkflowAdapter implements ReferenceData
 
     @Override
     public ReferenceDataDTO startWorkflow(ReferenceDataDTO referenceDataDTO) {
-        Long userId = DetailsHelper.getUserDetails().getUserId();
-        String employeeNum = EmployeeHelper.getEmployeeNum(userId, referenceDataDTO.getTenantId());
-        if (StringUtils.isBlank(employeeNum)) {
-            // TODO 异常
-            throw new CommonException("The current user does not maintain employee information");
+        lock.lock();
+        try {
+            Long userId = DetailsHelper.getUserDetails().getUserId();
+            String employeeNum = EmployeeHelper.getEmployeeNum(userId, referenceDataDTO.getTenantId());
+            if (StringUtils.isBlank(employeeNum)) {
+                // TODO 异常
+                throw new CommonException("The current user does not maintain employee information");
+            }
+            Long dataId = referenceDataDTO.getDataId();
+            // 先校验当前参考数据是否在流程中
+            int count = referenceDataRecordRepository.selectCountByCondition(Condition.builder(ReferenceDataRecord.class)
+                    .andWhere(Sqls.custom()
+                            .andEqualTo(ReferenceDataRecord.FIELD_DATA_ID, dataId, true)
+                            .andEqualTo(ReferenceDataRecord.FIELD_RECORD_STATUS, ReferenceDataConstant.RUNNING))
+                    .build());
+            if (count > 0) {
+                // TODO 异常
+                throw new CommonException("The current data is already in the process");
+            }
+            // 先删除原来的记录
+            referenceDataRecordRepository.delete(ReferenceDataRecord.builder()
+                    .dataId(dataId)
+                    // .recordType(ReferenceDataConstant.RELEASE)
+                    .build());
+            // 新增记录
+            ReferenceDataRecord dataRecord = ReferenceDataRecord
+                    .builder()
+                    .dataId(dataId)
+                    .recordType(ReferenceDataConstant.RELEASE)
+                    .recordStatus(ReferenceDataConstant.RUNNING)
+                    .applyUserId(userId)
+                    .build();
+            referenceDataRecordRepository.insertSelective(dataRecord);
+            Map<String, Object> workflowParams = new HashMap<>(4);
+            workflowParams.put(ReferenceDataConstant.RESPONSIBLE_PERSON, referenceDataDTO.getResponsiblePersonCode());
+            workflowParams.put(ReferenceDataConstant.RECORD_ID, dataRecord.getRecordId());
+            workflowParams.put(ReferenceDataConstant.DATA_ID, referenceDataDTO.getDataId());
+            String businessKey = String.valueOf(System.currentTimeMillis());
+            RunInstance runInstance = workflowClient.startInstanceByFlowKey(referenceDataDTO.getTenantId(), ReferenceDataConstant.RELEASE_WORKFLOW, businessKey, "EMPLOYEE", employeeNum, workflowParams);
+            dataRecord.setInstanceId(runInstance.getInstanceId());
+            referenceDataDTO.setDataStatus(ReferenceDataConstant.RELEASE_ING);
+            // 回写
+            referenceDataRecordRepository.updateOptional(dataRecord, ReferenceDataRecord.FIELD_INSTANCE_ID);
+            referenceDataRepository.updateDTOOptional(referenceDataDTO, ReferenceData.FIELD_DATA_STATUS);
+            return referenceDataDTO;
+        } finally {
+            lock.unlock();
         }
-        Long dataId = referenceDataDTO.getDataId();
-        // 先校验当前参考数据是否在流程中
-        int count = referenceDataRecordRepository.selectCountByCondition(Condition.builder(ReferenceDataRecord.class)
-                .andWhere(Sqls.custom()
-                        .andEqualTo(ReferenceDataRecord.FIELD_DATA_ID, dataId, true)
-                        .andEqualTo(ReferenceDataRecord.FIELD_RECORD_STATUS, ReferenceDataConstant.RUNNING))
-                .build());
-        if (count > 0) {
-            // TODO 异常
-            throw new CommonException("The current data is already in the process");
-        }
-        // 先删除原来的记录
-        referenceDataRecordRepository.delete(ReferenceDataRecord.builder()
-                .dataId(dataId)
-                // .recordType(ReferenceDataConstant.RELEASE)
-                .build());
-        // 新增记录
-        ReferenceDataRecord dataRecord = ReferenceDataRecord
-                .builder()
-                .dataId(dataId)
-                .recordType(ReferenceDataConstant.RELEASE)
-                .recordStatus(ReferenceDataConstant.RUNNING)
-                .applyUserId(userId)
-                .build();
-        referenceDataRecordRepository.insertSelective(dataRecord);
-        Map<String, Object> workflowParams = new HashMap<>(4);
-        workflowParams.put(ReferenceDataConstant.RESPONSIBLE_PERSON, referenceDataDTO.getResponsiblePersonCode());
-        workflowParams.put(ReferenceDataConstant.RECORD_ID, dataRecord.getRecordId());
-        workflowParams.put(ReferenceDataConstant.DATA_ID, referenceDataDTO.getDataId());
-        String businessKey = String.valueOf(System.currentTimeMillis());
-        RunInstance runInstance = workflowClient.startInstanceByFlowKey(referenceDataDTO.getTenantId(), ReferenceDataConstant.RELEASE_WORKFLOW, businessKey, "EMPLOYEE", employeeNum, workflowParams);
-        dataRecord.setInstanceId(runInstance.getInstanceId());
-        referenceDataDTO.setDataStatus(ReferenceDataConstant.RELEASE_ING);
-        // 回写
-        referenceDataRecordRepository.updateOptional(dataRecord, ReferenceDataRecord.FIELD_INSTANCE_ID);
-        referenceDataRepository.updateDTOOptional(referenceDataDTO, ReferenceData.FIELD_DATA_STATUS);
-        return referenceDataDTO;
     }
 
     @Override
