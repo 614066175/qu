9860e78cef0da57522b01843d6bfae910df900c2
lgl <768472457@qq.com>
2023-05-26 11:04:10 +0800

    [fix] 不等于，考虑null值

---
+++ src/main/java/com/hand/hdsp/quality/infra/converttype/ConvertTypeDoris.java
@@ -0,0 +1,15 @@
+package com.hand.hdsp.quality.infra.converttype;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * description
+ *
+ * @author 23168 2021/08/27 11:31
+ */
+@Component("DORIS")
+public class ConvertTypeDoris extends ConvertTypeMysql{
+
+}
