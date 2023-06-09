568ed9259756806747e3d286bac837849a04dbaa
29702 <fuqiang.luo@hand-china.com>
2023-06-09 17:11:55 +0800

    [fix] 参考数据错误多语言&未维护参考值不允许发布

--- src/main/java/com/hand/hdsp/quality/infra/constant/ErrorCode.java
+++ src/main/java/com/hand/hdsp/quality/infra/constant/ErrorCode.java
@@ -216,4 +216,12 @@ public interface ErrorCode {
     String ROOT_NOT_EXIST = "hdsp.xqua.err.root_not_exists";
 
     String GROUP_HAS_ROOT = "hdsp.xqua.err.group_has_root";
+
+    String USER_NO_EMPLOYEE = "xqua.err.user_no_employee";
+
+    String DATA_IN_PROCESS = "xqua.err.data_in_process";
+
+    String DATA_NO_VALUE = "xqua.err.data_no_value";
+
+
 }
