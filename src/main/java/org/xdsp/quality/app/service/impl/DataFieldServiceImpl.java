489123b0f312afbfbdac52046aef3ec904d1aa7f
lgl <768472457@qq.com>
2023-10-08 18:28:56 +0800

    [fix] 字段标准发布

--- src/main/java/org/xdsp/quality/app/service/impl/DataFieldServiceImpl.java
+++ src/main/java/org/xdsp/quality/app/service/impl/DataFieldServiceImpl.java
@@ -396,11 +396,11 @@ public class DataFieldServiceImpl implements DataFieldService {
                 dataFieldOnlineWorkflowAdapter.startWorkflow(dataFieldDTO);
             } else {
                 dataFieldDTO.setObjectVersionNumber(dto.getObjectVersionNumber());
-                //存版本表
-                doVersion(dataFieldDTO);
                 dataFieldDTO.setReleaseBy(DetailsHelper.getUserDetails().getUserId());
                 dataFieldDTO.setReleaseDate(new Date());
                 dataFieldRepository.updateDTOOptional(dataFieldDTO, DataField.FIELD_STANDARD_STATUS, DataField.FIELD_RELEASE_BY, DataField.FIELD_RELEASE_DATE);
+                //存版本表
+                doVersion(dataFieldDTO);
                 return ;
             }
         }
